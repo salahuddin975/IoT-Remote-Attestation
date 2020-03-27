86ed539113b4baa3edeb9c68b1bdfe04b29eb35c
salahuddin975 <salahuddin975@gmail.com>
2020-03-27 06:19:04 +0000

    Update project compilation script.

--- compile.sh
+++ compile.sh
@@ -3,27 +3,27 @@
 # Compile Irrigation Server
 echo "Compiling Irrigation Server"
 cd IrrigationServer
-gcc -o IrrigationServer server.cpp -fno-stack-protector -z execstack
-chmod +x so.py ho.py
+make clean; make
+chmod +x so.py ret_2_libc.py ho.py so_rev.py rev_ret2libc.py  ho_rev.py 
 cd ..
 
 # install Smart Home
 echo "Compiling Smart Home"
 cd smarthome
-gcc -o SmartHome main.c -l bcm2835 -fno-stack-protector -z execstack
-chmod +x so.py ho.py
+./compile.sh
+chmod +x so.py ret_2_libc.py ho.py so_rev.py rev_ret2libc.py  ho_rev.py
 cd ..
 
 # install Remote Control Monitor Car Server
 sudo apt-get install libopencv-dev
 cd remote-control-monitor-car-server/src/
 make clean; make
-chmod +x so.py ho.py
+chmod +x so.py ret_2_libc.py ho.py so_rev.py rev_ret2libc.py  ho_rev.py
 cd ../..
 
 #install DNS server
 cd DNS_server_raspberrypi
 make clean; make
-chmod +x so.py ho.py
+chmod +x so.py ret_2_libc.py ho.py so_rev.py rev_ret2libc.py  ho_rev.py
 cd ..
 
