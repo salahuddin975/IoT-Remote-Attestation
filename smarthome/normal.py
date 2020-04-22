3c61f90d7c1ec1aa02539def1312696f81e1aec0
salahuddin975 <salahuddin975@gmail.com>
2020-04-22 07:20:19 +0100

    script to send normal message.

---
+++ smarthome/normal.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import sys, socket
+
+shellcode = ( "AAAA")
+
+sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM,0)
+sock.connect(( sys.argv[1], int(sys.argv[2]) ))
+sock.send(shellcode)
+sock.close()
+
+print "Shellcode Length: %d" %len(shellcode)
