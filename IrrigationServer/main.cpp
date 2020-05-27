bd1b88866fd4ff6770c5549b85c89b61d58da34b
salahuddin975 <salahuddin975@gmail.com>
2020-05-27 22:22:12 +0100

    self-checksum for Irrigation server.

--- IrrigationServer/main.cpp
+++ IrrigationServer/main.cpp
@@ -7,7 +7,26 @@
 #include <map>
 #include <vector>
 #include <time.h>
+#include <pthread.h>
+#include <iostream>
+#include <link.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+
+#include "xxhash.h"
 #include "server.hpp"
+
+
+#define CHECKSUM_PORT     8080 
+#define MAXLINE 1024 
+
 
 Server s;
 extern int attack_type = 0;
@@ -22,7 +41,91 @@ void SignalHandler(int signum)
     s.Terminate();
 }
 
+static int checksum_callback(struct dl_phdr_info *info, size_t size, void *data)
+{
+    XXH64_state_t* state = (XXH64_state_t*)data;
+    for (int header_id = 0; header_id < info->dlpi_phnum; header_id++) {
+        const ElfW(Phdr) *hdr = &info->dlpi_phdr[header_id];
+        // Skip over headers that can not include read-only data that
+        // possibly is also executable code:
+        if (hdr->p_memsz == 0) {
+            continue;
+        }
+        if ((hdr->p_flags & (PF_R | PF_X)) != (PF_R | PF_X)) {
+            continue;
+        }
+        if (hdr->p_flags & PF_W) {
+            continue;
+        }
 
+        XXH_errorcode result = XXH64_update(
+            state,
+            (void*)(info->dlpi_addr + hdr->p_vaddr),
+            hdr->p_memsz);
+        if (result != XXH_OK) {
+            abort();
+        }
+    }
+    return 0;
+}
+
+void calculate_checksum(char* hash_value)
+{
+    XXH64_state_t* state = XXH64_createState();
+    if (state == NULL) {
+        fprintf(stderr, "Unable to allocate xxHash state!\n");
+        return EXIT_FAILURE;
+    }
+    if (XXH64_reset(state, 0) != XXH_OK) {
+        fprintf(stderr, "Failed to zero xxHash state!\n");
+        return EXIT_FAILURE;
+    }
+    dl_iterate_phdr(checksum_callback, state);
+    XXH64_hash_t digest = XXH64_digest (state);
+    XXH64_canonical_t canonical;
+    XXH64_canonicalFromHash(&canonical, digest);
+    
+    for (size_t byte_id = 0; byte_id < sizeof(canonical.digest); byte_id++) {
+        printf("%02x", canonical.digest[byte_id]);
+    }
+    printf("\n");
+
+    strncpy(hash_value, canonical.digest, 8);
+    hash_value[8] = '\0';
+    
+    XXH64_freeState(state);
+}
+
+
+void *checksum(void *vargp)
+{
+    int sockfd; 
+    char hash_value[9];
+    char buffer[MAXLINE]; 
+    struct sockaddr_in servaddr, cliaddr; 
+      
+    memset(&servaddr, 0, sizeof(servaddr)); 
+    memset(&cliaddr, 0, sizeof(cliaddr)); 
+      
+    servaddr.sin_family    = AF_INET; 
+    servaddr.sin_addr.s_addr = INADDR_ANY; 
+    servaddr.sin_port = htons(CHECKSUM_PORT); 
+      
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0); 
+    
+    if(bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 ) { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+      
+    int len, n; 
+    len = sizeof(cliaddr);
+
+    while(recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len)){
+	calculate_checksum(hash_value);
+        sendto(sockfd, (const char *)hash_value, strlen(hash_value), MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len); 
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -32,6 +135,12 @@ int main(int argc, char **argv)
         }
 
         attack_type = atoi(argv[1]);
+
+	pthread_t tid;
+        pthread_create(&tid, NULL, checksum, NULL);
+	
+	std::cout << "running main program." << std::endl;
+
 
     atexit(TerminationHandler);
     if (signal (SIGINT, SignalHandler) == SIG_IGN)
