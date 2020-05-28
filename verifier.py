7c269d45284fd0a9b37547fb181d1b789d389818
salahuddin975 <salahuddin975@gmail.com>
2020-05-28 05:57:13 +0100

    add verifier.

--- verifier.py
+++ verifier.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
+
 import socket
 import sys
+import binascii
 
 
 IRRIGATION_SERVER_IP = '192.168.1.4'
@@ -8,24 +11,23 @@ IRRIGATION_SERVER_PORT = 8080
 SMARTHOME_SERVER_IP = '192.168.1.4'
 SMARTHOME_SERVER_PORT = 8081
 
-RCCM_SERVER_IP = '192.168.1.4'
-RCCM_SERVER_PORT = 8082
-
 DNS_SERVER_IP = '192.168.1.4'
-DNS_SERVER_PORT = 8083
+DNS_SERVER_PORT = 8082
+
+RCCM_SERVER_IP = '192.168.1.4'
+RCCM_SERVER_PORT = 8083
 
 
-
-message = "calculate checksum"
+message = b"calculate checksum"
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while 1:
-    print "\n\nPlease select server to get checksum value: "
-    print "Irrigation Server --> 1"
-    print "Smart Home --> 2"
-    print "Remote Control Car Monitor --> 3"
-    print "DNS server --> 4"
-    print "Exit from the program --> 5"
+    print ("\n\nPlease select server to get checksum value: ")
+    print ("Irrigation Server --> 1")
+    print ("Smart Home --> 2")
+    print ("DNS server --> 3")
+    print ("Remote Control Car Monitor --> 4")
+    print ("Exit from the program --> 5")
 
     val = int(input())
 
@@ -34,16 +36,16 @@ while 1:
     elif val == 2:
         server_address = (SMARTHOME_SERVER_IP, SMARTHOME_SERVER_PORT)
     elif val == 3:
-        server_address = (RCCM_SERVER_IP, RCCM_SERVER_PORT)
-    elif val == 4:
         server_address = (DNS_SERVER_IP, DNS_SERVER_PORT)
+    elif val == 4:
+        server_address = (RCCM_SERVER_IP, RCCM_SERVER_PORT)
     else:
         print ("exit")
         sys.exit()
     
     sent = sock.sendto(message, server_address)
     data, server = sock.recvfrom(4096)
-    print ('Checksum: ', data)
+    print ("Checksum: ", binascii.hexlify(data))
 
 
 sock.close()
