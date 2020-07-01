042a83a561d792cc4eab0fc6848d4dbc1434182f
salahuddin975 <salahuddin975@gmail.com>
2020-07-01 07:24:13 +0100

    Add randome checksum calculation for SmartHome server.

--- smarthome/main.c
+++ smarthome/main.c
@@ -14,11 +14,13 @@
 #include <sys/socket.h> 
 #include <arpa/inet.h> 
 #include <netinet/in.h> 
-#include "xxhash.h"
+#include <openssl/sha.h>
 #include "bcm2835.h"
 
+//#include "xxhash.h"
 
-#define CHECKSUM_PORT     8081
+
+#define CHECKSUM_PORT     8080
 #define MAXLINE 1024 
 
 
@@ -39,6 +41,7 @@ void lamp_on(int num);
 void lamp_off(int num);
 void accept_command(int mainSocket, char* string);
 
+/*
 static int checksum_callback(struct dl_phdr_info *info, size_t size, void *data)
 {
     XXH64_state_t* state = (XXH64_state_t*)data;
@@ -94,11 +97,15 @@ void calculate_checksum(char* hash_value)
     
     XXH64_freeState(state);
 }
+*/
+
+void calculate_checksum(char *hash_value, unsigned int seed);
+
 
 void *checksum(void *vargp)
 {
     int sockfd; 
-    char hash_value[9];
+    char hash_value[SHA256_DIGEST_LENGTH];
     char buffer[MAXLINE]; 
     struct sockaddr_in servaddr, cliaddr; 
 
@@ -119,8 +126,11 @@ void *checksum(void *vargp)
     int len, n; 
     len = sizeof(cliaddr);
 
+    time_t t;
+    unsigned int seed = (unsigned) time(&t);
+
     while(recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len)){
-        calculate_checksum(hash_value);
+        calculate_checksum(hash_value, seed);
         sendto(sockfd, (const char *)hash_value, strlen(hash_value), MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len); 
     }
  
