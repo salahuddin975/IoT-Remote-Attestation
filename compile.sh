f1f35393785f9fdf39df7a514bcc7d2c580f7482
salahuddin975 <salahuddin975@gmail.com>
2020-07-03 20:56:31 +0100

    remove carMonitor server from compile.sh.

--- compile.sh
+++ compile.sh
@@ -19,13 +19,6 @@ echo "Compiling Smart Home"
 chmod +x so.py ret_2_libc.py ho.py so_rev.py rev_ret2libc.py  ho_rev.py
 cd ..
 
-# install Remote Control Monitor Car Server
-sudo apt-get install libopencv-dev
-cd remote-control-monitor-car-server/src/
-make clean; make
-chmod +x so.py ret_2_libc.py ho.py so_rev.py rev_ret2libc.py  ho_rev.py
-cd ../..
-
 #install DNS server
 cd DNS_server_raspberrypi
 make clean; make
