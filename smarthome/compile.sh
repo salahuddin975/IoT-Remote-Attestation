a9fae11412ac4cb5234c38eeb0cfddf6c1b4863d
salahuddin975 <salahuddin975@gmail.com>
2020-07-23 18:24:46 +0100

    modify compile.sh file of smarthome server for clang.:

--- smarthome/compile.sh
+++ smarthome/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ main.c checksum.cpp -l bcm2835 -fno-stack-protector -zexecstack -lpthread -fpermissive -lssl -lcrypto   -o SmartHome
+clang++  -I/usr/include/c++/8 -I/usr/include/arm-linux-gnueabihf/c++/8/ -L /usr/lib/gcc/arm-linux-gnueabihf/8/  main.cpp checksum.cpp -l bcm2835 -fno-stack-protector -z execstack -lpthread -fpermissive -lssl -lcrypto   -o SmartHome
