12a5ed85b6d16bd116e5c9bcc200dbf703fdbf10
salahuddin975 <salahuddin975@gmail.com>
2020-08-21 17:59:22 +0100

    change binary location.

--- smarthome/compile-clang.sh
+++ smarthome/compile-clang.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-clang  -I/usr/include/c++/8 src/main.c src/checksum.c -lbcm2835 -fno-stack-protector -z execstack -lpthread -fpermissive -lssl -lcrypto   -o build/Smarthome-clang
+clang  -I/usr/include/c++/8 src/main.c src/checksum.c -lbcm2835 -fno-stack-protector -z execstack -lpthread -fpermissive -lssl -lcrypto -o Smarthome-clang
--- smarthome/compile-gcc.sh
+++ smarthome/compile-gcc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc  -I/usr/include/c++/8 src/main.c src/checksum.c -lbcm2835 -fno-stack-protector -z execstack -lpthread -fpermissive -lssl -lcrypto   -o build/Smarthome-gcc
+gcc  -I/usr/include/c++/8 src/main.c src/checksum.c -lbcm2835 -fno-stack-protector -z execstack -lpthread -fpermissive -lssl -lcrypto -o Smarthome-gcc
